787af2bd2066593255173db5b03404d535323431
notaprofesssor <124461031+notaprofesssor@users.noreply.github.com>
2023-02-09 21:55:30 +0300

    Create secretmessage translater.js

---
+++ secretmessage translater.js
@@ -0,0 +1,35 @@
+let secretMessage = [
+  "Learning",
+  "is",
+  "not",
+  "about",
+  "what",
+  "you",
+  "get",
+  "easily",
+  "the",
+  "first",
+  "time,",
+  "it",
+  "is",
+  "about",
+  "what",
+  "you",
+  "can",
+  "figure",
+  "out.",
+  "-2015,",
+  "Chris",
+  "Pine,",
+  "Learn",
+  "JavaScript",
+];
+console.log(secretMessage.length);
+secretMessage.pop();
+console.log(secretMessage.length);
+secretMessage.push("to", "Program");
+secretMessage[7] = "right";
+const firstElement = secretMessage.shift();
+secretMessage.unshift("Programming");
+secretMessage.splice(6, 11, "know");
+console.log(secretMessage.join(' '));
